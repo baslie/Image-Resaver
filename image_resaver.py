ff2620a41452850dedf8b7656316847a8d44f9b5
Roman Purtov <rytrycon@gmail.com>
2024-11-22 16:36:18 +0700

    Update image_resaver.py

--- image_resaver.py
+++ image_resaver.py
@@ -1,9 +1,42 @@
 import sys
 import os
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QProgressBar
 from PyQt5.QtGui import QFont, QPalette, QColor, QIcon, QFontDatabase
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QThread, pyqtSignal
 from PIL import Image
+
+class ImageProcessor(QThread):
+    progress = pyqtSignal(int)
+    finished = pyqtSignal(str)
+
+    def __init__(self, files):
+        super().__init__()
+        self.files = files
+
+    def run(self):
+        image_count = 0
+        total_images = min(len(self.files), 50)
+        for index, file_path in enumerate(self.files):
+            if image_count >= 50:
+                break  # Прерываем без предупреждения, если достигли 50 файлов
+            if os.path.isfile(file_path):
+                ext = os.path.splitext(file_path)[1].lower()
+                if ext in ['.jpg', '.jpeg', '.png']:
+                    try:
+                        image = Image.open(file_path)
+                        if ext in ['.jpg', '.jpeg']:
+                            image.save(file_path, 'JPEG', quality=100)
+                        elif ext == '.png':
+                            image.save(file_path, 'PNG', optimize=True)
+                        image_count += 1
+                    except Exception:
+                        pass  # Игнорируем ошибки без вывода предупреждений
+                else:
+                    pass  # Игнорируем неподдерживаемые файлы без предупреждений
+            # Обновляем прогресс
+            progress_value = int(((index + 1) / total_images) * 100)
+            self.progress.emit(progress_value)
+        self.finished.emit('Готово! Изображения пересохранены.')
 
 class ImageResaver(QWidget):
     def __init__(self):
@@ -37,8 +70,27 @@ class ImageResaver(QWidget):
         self.label.setAlignment(Qt.AlignCenter)
         self.label.setWordWrap(True)  # Разрешаем перенос строк для длинного текста
 
+        # Прогресс-бар
+        self.progress_bar = QProgressBar(self)
+        self.progress_bar.setValue(0)
+        self.progress_bar.setVisible(False)
+        self.progress_bar.setTextVisible(True)
+        self.progress_bar.setAlignment(Qt.AlignCenter)
+        self.progress_bar.setStyleSheet("""
+            QProgressBar {
+                border: 2px solid grey;
+                border-radius: 5px;
+                text-align: center;
+            }
+            QProgressBar::chunk {
+                background-color: #05B8CC;
+                width: 20px;
+            }
+        """)
+
         layout = QVBoxLayout()
         layout.addWidget(self.label)
+        layout.addWidget(self.progress_bar)
         self.setLayout(layout)
 
     def dragEnterEvent(self, event):
@@ -56,25 +108,21 @@ class ImageResaver(QWidget):
         self.processFiles(files)
 
     def processFiles(self, files):
-        image_count = 0
-        for file_path in files:
-            if image_count >= 50:
-                break  # Прерываем без предупреждения, если достигли 50 файлов
-            if os.path.isfile(file_path):
-                ext = os.path.splitext(file_path)[1].lower()
-                if ext in ['.jpg', '.jpeg', '.png']:
-                    try:
-                        image = Image.open(file_path)
-                        if ext in ['.jpg', '.jpeg']:
-                            image.save(file_path, 'JPEG', quality=100)
-                        elif ext == '.png':
-                            image.save(file_path, 'PNG', optimize=True)
-                        image_count += 1
-                    except Exception:
-                        pass  # Игнорируем ошибки без вывода предупреждений
-                else:
-                    pass  # Игнорируем неподдерживаемые файлы без предупреждений
-        self.label.setText('Готово! Изображения пересохранены.')
+        self.label.setText('Обработка изображений...')
+        self.progress_bar.setVisible(True)
+        self.progress_bar.setValue(0)
+
+        self.thread = ImageProcessor(files)
+        self.thread.progress.connect(self.updateProgress)
+        self.thread.finished.connect(self.processingFinished)
+        self.thread.start()
+
+    def updateProgress(self, value):
+        self.progress_bar.setValue(value)
+
+    def processingFinished(self, message):
+        self.label.setText(message)
+        self.progress_bar.setVisible(False)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
